610f526f5cc062af3b0bd03b7b439bee04386c5d
HannaFarha <johnnyfarha1@gmail.com>
2023-12-22 17:59:28 +0100

    header

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+const Footer = () => {
+   
+  
+    return (
+      <h1>Hallo word</h1>
+    );
+  };
+  
+  export default Footer;
+  
---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+    return ( <>jdj</> );
+}
+ 
+export default Navbar;
