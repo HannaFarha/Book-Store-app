c4ae9c03c4ce1c4184f0975d07c6bf01cfd57634
HannaFarha <johnnyfarha1@gmail.com>
2024-01-02 14:19:53 +0100

    authors done

--- src/pages/authors/Authors.jsx
+++ src/pages/authors/Authors.jsx
@@ -1,5 +1,53 @@
-const Author = () => {
-    return ( <h1>Authors</h1> );
+import { useState,useEffect } from "react";
+import "./authors.css";
+
+const Authors = () => {
+  const [search, setSearch] = useState("");
+
+
+    const [authors,setAuthors]=useState([]);
+const fetchBooks=async()=>{
+    try{
+        const response =await fetch('http://localhost:4000/authors')
+        if(response.ok){
+            const booksData =await response.json()
+            setAuthors(booksData)
+        }
+
+    }
+    catch(error){
+        console.log(error)
+    }
 }
+useEffect(()=>{fetchBooks()},[])
+
+
+    return ( <section className="authors">
+    <div className="authors-search-wrapper">
+      <input
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        type="search"
+        placeholder="Search in authors"
+      />
+    </div>
+    <div className="authors-wrapper">
+      {authors
+        .filter(a => a.name.toLowerCase().includes(search))
+        .map((author) => (
+          <div key={author.id} className="author">
+            <img
+              src={author.image}
+              alt={author.name}
+              className="author-img"
+            />
+            <h2 className="author-name">{author.name}</h2>
+          </div>
+        ))}
+    </div>
+  </section>
+);
+};
+
  
-export default Author;
+export default Authors;
