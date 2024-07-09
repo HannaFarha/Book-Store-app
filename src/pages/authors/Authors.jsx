8eb6ae319a9eacfa28e509a3f582ac8a61d63c8f
HannaFarha <johnnyfarha1@gmail.com>
2024-07-09 15:07:06 +0200

    almost fixed

--- src/pages/authors/Authors.jsx
+++ src/pages/authors/Authors.jsx
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react";
 import "./authors.css";
-import { authors } from "../../components/data/author.js";
+import { authors } from "../../data/author";
 const Authors = () => {
   const [search, setSearch] = useState("");
 
