fab2cabcec1f9278f954dfd4229f65f544667535
HannaFarha <johnnyfarha1@gmail.com>
2024-07-10 13:01:35 +0200

    finish

--- src/pages/forms/Register.jsx
+++ src/pages/forms/Register.jsx
@@ -11,11 +11,11 @@ const Register = () => {
   const [books,setBooks]=useState([]);
   const [oldData,setOldData]=useState();
   console.log(oldData)
-  const handleDelete = async (id) => {
-    await axios.delete(`http://localhost:4000/emails/${id}`);
-    alert('Data deleted successfully');
-    fetchBooks()
-  }
+  // const handleDelete = async (id) => {
+  //   await axios.delete(`http://localhost:4000/emails/${id}`);
+  //   alert('Data deleted successfully');
+  //   fetchBooks()
+  // }
     
 const dataEdit = async (id) => {
   
