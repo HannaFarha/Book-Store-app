878b05b76b4c0f9b3ed62df31d15b1929f8a235d
HannaFarha <johnnyfarha1@gmail.com>
2026-02-21 13:56:43 +0100

    First

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,23 +2,41 @@ import "./services.css";
 
 const Services = () => {
   return (
-    //all are the same and can be changed
     <div className="services">
+      {/* خدمة 1: الشحن */}
       <div className="service-item">
         <i className="bi bi-truck"></i>
-        <b>Free Shipping</b>
+        <div className="service-text">
+          <b>توصيل سريع ومجاني</b>
+          <span>لجميع الطلبات داخل المملكة</span>
+        </div>
       </div>
+
+      {/* خدمة 2: الدعم */}
+      <div className="service-item">
+        <i className="bi bi-headset"></i> {/* أيقونة سماعة رأس أنسب للدعم */}
+        <div className="service-text">
+          <b>دعم فني متواصل</b>
+          <span>خدمة عملاء 24/7</span>
+        </div>
+      </div>
+
+      {/* خدمة 3: الاسترجاع */}
+      <div className="service-item">
+        <i className="bi bi-arrow-repeat"></i> {/* أيقونة استرجاع */}
+        <div className="service-text">
+          <b>ضمان استرجاع</b>
+          <span>استرجاع سهل خلال 7 أيام</span>
+        </div>
+      </div>
+
+      {/* خدمة 4: الهدايا */}
       <div className="service-item">
         <i className="bi bi-gift"></i>
-        <b>Gift Card</b>
-      </div>
-      <div className="service-item">
-        <i className="bi bi-arrow-clockwise"></i>
-        <b>7 Days Return</b>
-      </div>
-      <div className="service-item">
-        <i className="bi bi-send"></i>
-        <b>Contact Us</b>
+        <div className="service-text">
+          <b>بطاقات هدايا</b>
+          <span>اسرق قلوب أحبائك بهدايا مميزة</span>
+        </div>
       </div>
     </div>
   );
